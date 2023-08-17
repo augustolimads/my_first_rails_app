8c7414a9cff4211d3200ca79c5da028db76bb3af
Augusto Lima <augustolimads@gmail.com>
2023-08-16 21:30:20 -0300

    feature: add friends

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_22_160530) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_06_231338) do
   create_table "cars", force: :cascade do |t|
     t.string "make"
     t.string "model"
     t.integer "year"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "friends", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "email"
+    t.string "phone"
+    t.string "twitter"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
